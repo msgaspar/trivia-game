98d261ee59ee0b089ea09799d93a4bbb08104215
Matheus Gaspar <msouzagaspar@gmail.com>
2021-07-16 15:38:54 -0300

    adiciona dados e botoes na pagina de feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,26 +1,57 @@
 import React, { Component } from 'react';
-import { connect } from 'redux';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { assertions, score, history } = this.props;
+    const minimumAssertions = 3;
+
     return (
       <div>
         <Header />
         <div>
-          <h1>Podia ser melhor...</h1>
-          <p>Você acertou 2 questões!</p>
-          <p>Um total de 20 pontos</p>
+          <h1 data-testid="feedback-text">
+            {assertions < minimumAssertions ? 'Podia ser melhor...' : 'Mandou bem!'}
+          </h1>
+          <p
+            data-testid="feedback-total-question"
+          >
+            {`Você acertou ${assertions} questões!`}
+          </p>
+          <p data-testid="feedback-total-score">{`Um total de ${score} pontos`}</p>
         </div>
-        <button type="button">VER RANKING</button>
-        <button type="button">JOGAR NOVAMENTE</button>
+        <button
+          type="button"
+          data-testid="btn-ranking"
+          onClick={ () => history.push('/ranking') }
+        >
+          VER RANKING
+        </button>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ () => history.push('/') }
+        >
+          JOGAR NOVAMENTE
+        </button>
       </div>
     );
   }
 }
 
-const mapStateToProps = () => ({
-
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+  score: state.player.score,
 });
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default connect(mapStateToProps)(Feedback);
